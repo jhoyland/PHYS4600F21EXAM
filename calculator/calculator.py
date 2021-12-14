fb89b1ad5f3d4c0d52987e566c56dd69c35484bc
ernie-ng <ernie_ng@outlook.com>
2021-12-14 11:35:00 -0800

    Exam Finished

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,8 +1,89 @@
 import tkinter as tk
+import numpy as np
+import math
 
+
+
+expression = "" # 1+1
+
+def button_press(num):
+    global expression
+    expression = expression + str(num) #expression variable stores the entered elements one after another.
+    var_eq.set(expression) #sets the tk varibale as the expression
+    return
+
+
+def calculate():
+    global expression
+    result = str(eval(expression))  #python built in evaluate function.
+    var_eq.set(result)
+    return    
+    
+def clear():
+    global expression
+    expression = ""
+    var_eq.set(expression) #empty the box
+    return
+    
 root = tk.Tk()
 root.wm_title("Calculator exam")
 
+ctrlframe = tk.Frame(root)
+ctrlframe.pack(side=tk.LEFT)
+
+
 # Definitions of your GUI elements and functions go here
+
+label_display = tk.Label(ctrlframe,text="display",width=20)
+var_eq = tk.StringVar(value="")
+entry_display = tk.Entry(ctrlframe,text="display",textvariable=var_eq,width=20)
+entry_display.grid(row=0, column=1,padx=(0,10),pady=(0,10))
+
+#needed lambda python function for button presses to work.
+num1_button = tk.Button(ctrlframe,text="1",command = lambda: button_press(1))
+num1_button.grid(row=2,column=0,columnspan=1)
+
+num2_button = tk.Button(ctrlframe,text="2",command = lambda: button_press(2))
+num2_button.grid(row=2,column=1,columnspan=1)
+
+num3_button = tk.Button(ctrlframe,text="3",command = lambda: button_press(3))
+num3_button.grid(row=2,column=2,columnspan=1)
+
+num4_button = tk.Button(ctrlframe,text="4",command = lambda: button_press(4))
+num4_button.grid(row=3,column=0,columnspan=1)
+
+num5_button = tk.Button(ctrlframe,text="5",command = lambda: button_press(5))
+num5_button.grid(row=3,column=1,columnspan=1)
+
+num6_button = tk.Button(ctrlframe,text="6",command = lambda: button_press(6))
+num6_button.grid(row=3,column=2,columnspan=1)
+
+num7_button = tk.Button(ctrlframe,text="7",command = lambda: button_press(7))
+num7_button.grid(row=4,column=0,columnspan=1)
+
+num8_button = tk.Button(ctrlframe,text="8",command = lambda: button_press(8))
+num8_button.grid(row=4,column=1,columnspan=1)
+
+num9_button = tk.Button(ctrlframe,text="9",command = lambda: button_press(9))
+num9_button.grid(row=4,column=2,columnspan=1)
+
+num0_button = tk.Button(ctrlframe,text="0",command = lambda: button_press(0))
+num0_button.grid(row=5,column=1,columnspan=1)
+
+
+add_button = tk.Button(ctrlframe,text="+",command= lambda: button_press("+"))
+add_button.grid(row=6,column=1,columnspan=2)
+
+subtract_button = tk.Button(ctrlframe,text="-",command= lambda: button_press("-"))
+subtract_button.grid(row=7,column=1,columnspan=2)
+
+equal_button = tk.Button(ctrlframe,text="=",command=calculate)
+equal_button.grid(row=8,column=1,columnspan=2)
+
+clear_button = tk.Button(ctrlframe,text="clear",command=clear)
+clear_button.grid(row=9,column=1,columnspan=2)
+
+
+
 
 tk.mainloop()
--- montecarlo/montecarlo.c
+++ montecarlo/montecarlo.c
@@ -6,23 +6,51 @@
 double randrange(double a, double b)
 {
   // TODO: Write a function to generate a random number between x0 and x1
+	//i'll say a as the lower number and b is the higher number.
+	double randnum = 0.0;
+	double range = b-a;
+
+	randnum = ((double)rand()*range)/(double)RAND_MAX + a;
+	return randnum;
 }
 
-double function(double x)
+double function(double x) //the random number goes in here.
 {
   return sin(x);
 }
 
-double Montecarlo(double a, double b, int N)
+double Montecarlo(double a, double b, int N) //this computes the LHS of the equation.
 {
   // TODO: Write a function which uses the Monte carlo method to integrate "function" between a and n
   // N is the number of values to average over
+	double sum  = 0.0;
+	double ans = 0.0;
+	for(int i = 0; i<=N; i++)
+	{
+		sum = sum + function(randrange(a,b));
+	}
+
+	ans = (b-a) * ((double)1.0/N) * sum;
+
+	return ans;
 }
 
 int main(int argc, char** argv)
 {
   srand(time(0)); // This seeds the random number generator and should be called just once here.
-
   // Put your code here to run the Montecarlo integration for different values of N
-  // How big N is required for the integral to converge?
+  // How big N is required for the integral to converge to within 1%?
+  //From symbolab, the definite integral of sin(x)dx from 0 to pi is 2.
+  //For consistent results within 1%, N needs to be at least 10000. There seems to be caveats that lies beyond 1% even with a very big N=10000000.
+  //Probably the random number.
+  int a=0;
+  int b=3.14159265358979323846;
+  int N=10000;
+  double ans=0.0;
+
+  ans = Montecarlo(a, b, N);
+
+  printf("%f\n", ans);
+
+  return 0;
 }
